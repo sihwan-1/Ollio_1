77381bdcc922b2ddd29950681ec32e6a8c5bcd28
sihwan-1 <rlatlghks0917@gmail.com>
2025-08-07 13:51:22 +0900

    섹션 5 작업중

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function Popup_close() {
 }
 Popup_close() ;
 
+$(window).on('scroll', function () {
+  let scrollTop = $(this).scrollTop();
+
+  if (scrollTop > 20) {
+    $('header').addClass('on');
+  } else {
+    $('header').removeClass('on');
+  }
+});
+
+
 function Coupon_close() {
   $('.coupon-close').click(function () {
     $('.coupon').addClass('close');
@@ -20,3 +31,49 @@ function Search_1() {
   });
 }
 Search_1();
+
+function Swiper_1() {
+  var swiper = new Swiper(".swiper-1", {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false 
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+Swiper_1();
+
+function Swiper_2() {
+  var swiper = new Swiper(".swiper-2", {
+    slidesPerView: 4,
+    spaceBetween: 32,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+Swiper_2();
+
+function Swiper_3() {
+  var swiper = new Swiper(".swiper-3", {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+Swiper_3();
+
